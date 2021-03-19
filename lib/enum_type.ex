2ee226ff3816dd72a845ef86d13b5699e22d3c91
Scott Ames-Messinger <scott@commoncurriculum.com>
2021-03-19 10:04:08 -0400

    fix: Load values as strings to serialize correctly
    Previously, when an enum would be loaded, it would be loaded as the. module name. When the schema was serialized, the module name wasn't changed to the string, causing the returned JSON to be something like:
    
    ```
    %{
      status: "Elixir.MyApp.Status.Published"
    }
    ```

--- lib/enum_type.ex
+++ lib/enum_type.ex
@@ -121,7 +121,7 @@ defmodule EnumType do
         def cast(unquote(option)), do: {:ok, unquote(option)}
         def cast(unquote(value)), do: {:ok, unquote(option)}
 
-        def load(unquote(value)), do: {:ok, unquote(option)}
+        def load(unquote(value)), do: {:ok, unquote(option).value}
 
         # Allow both querying by Module and setting a value to the Module when updating or inserting.
         def dump(unquote(option)), do: {:ok, unquote(option).value}
